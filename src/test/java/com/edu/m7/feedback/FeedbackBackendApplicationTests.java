08ca184bac9bb561fa0caaf2e0c529a872cd8fe7
proehr <pauline.roehr@yahoo.de>
2022-11-22 17:26:13 +0100

    imi-wise-2022/projectdocumentation#22 - Initial commit

---
+++ src/test/java/com/edu/m7/feedback/FeedbackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.m7.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
