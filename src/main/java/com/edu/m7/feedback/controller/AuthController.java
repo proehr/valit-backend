823f944e019eab0c18c9f204da82814b1258690b
Pauline <92150047+polygula@users.noreply.github.com>
2023-01-04 11:33:08 +0100

    imi-wise-2022/projectdocumentation#83 - use singular imports in AuthController

--- src/main/java/com/edu/m7/feedback/controller/AuthController.java
+++ src/main/java/com/edu/m7/feedback/controller/AuthController.java
@@ -20,7 +20,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
