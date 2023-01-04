1c06061c35d36fd836d8c392d32ed2e75c956aa4
Pauline <92150047+polygula@users.noreply.github.com>
2023-01-04 11:28:40 +0100

    imi-wise-2022/projectdocumentation#83 - add endpoint to check if user exists

--- src/main/java/com/edu/m7/feedback/controller/AuthController.java
+++ src/main/java/com/edu/m7/feedback/controller/AuthController.java
@@ -20,11 +20,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -82,5 +78,10 @@ public class AuthController {
         accountService.createUser(account);
 
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @GetMapping("/userExists")
+    public ResponseEntity<Boolean> userExists(@RequestBody String username) {
+        return ResponseEntity.ok(accountService.userExists(username));
     }
 }
