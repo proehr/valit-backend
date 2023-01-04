7d2568a960520c125213153fa63f1d81c584e644
henry.ordelt <henry.ordelt@student.htw-berlin.de>
2023-01-04 12:49:34 +0100

    add changes for authorization annotation, add endpoint for create and update lecturer

--- src/main/java/com/edu/m7/feedback/controller/LecturerController.java
+++ src/main/java/com/edu/m7/feedback/controller/LecturerController.java
@@ -4,18 +4,16 @@ import com.edu.m7.feedback.model.entity.Account;
 import com.edu.m7.feedback.model.entity.Lecturer;
 import com.edu.m7.feedback.model.repository.AccountRepository;
 import com.edu.m7.feedback.model.repository.LecturerRepository;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.edu.m7.feedback.payload.response.MessageResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 public class LecturerController {
 
     private final LecturerRepository repository;
@@ -25,13 +23,39 @@ public class LecturerController {
         this.repository = repository;
         this.accountRepository = accountRepository;
     }
+    Account getSignedInAccount(Principal principal) {
+        String username = principal.getName();
+        Optional<Account> optionalEntity = accountRepository.findByUsername(username);
+        return optionalEntity.orElseThrow();
+    }
 
-    @GetMapping("/profile")
-    Lecturer getLecturer() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
+
+    @RolesAllowed({"ROLE_LECTURER", "ROLE_ADMIN"})
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    @ResponseBody
+    Lecturer getLecturer(Principal principal) {
+        String username = principal.getName();
         Optional<Account> optionalEntity = accountRepository.findByUsername(username);
         Account account  = optionalEntity.orElseThrow();
         return repository.findByAccount(account).orElseThrow();
+    }
+
+    @RolesAllowed({"ROLE_LECTURER", "ROLE_ADMIN"})
+    @PostMapping("/lecturer")
+    ResponseEntity<MessageResponse> createOrUpdateLecturer(@RequestBody Lecturer newLecturer, Principal principal) {
+        Account account = getSignedInAccount(principal);
+        newLecturer.setAccount(account);
+         Optional<Lecturer> optionalLecturer = repository.findByAccount(account);
+        if (optionalLecturer.isPresent()) {
+            Lecturer lecturer = optionalLecturer.get();
+            newLecturer.setLecturerId(lecturer.getLecturerId());
+            newLecturer.setAccount(lecturer.getAccount());
+            repository.save(newLecturer);
+            return ResponseEntity.ok(new MessageResponse("Profile updated successfully!"));
+        } else {
+            repository.save(newLecturer);
+            return ResponseEntity.ok(new MessageResponse("Profile created successfully!"));
+        }
+
     }
 }
