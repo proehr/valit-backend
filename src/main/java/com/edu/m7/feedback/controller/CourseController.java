6148647762921e5b48659fa4502bd55ef6bcf23a
YasineDeghaies <yasine.deghaies@gmail.com>
2023-01-15 08:43:40 +0100

    imi-wise-2022/projectdocumentation#85 - Added CourseController and CourseService but I had to do change the type of the property 'dates' inside the 'Course' entity from LocalTime[] to String because LocalDate[] is not suppported by JPA. I'm working on a solution for this.

---
+++ src/main/java/com/edu/m7/feedback/controller/CourseController.java
@@ -0,0 +1,72 @@
+package com.edu.m7.feedback.controller;
+
+import com.edu.m7.feedback.model.entity.Course;
+import com.edu.m7.feedback.model.repository.LecturerRepository;
+import com.edu.m7.feedback.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseController(LecturerRepository lecturerRepository, CourseService courseService) {
+        this.lecturerRepository = lecturerRepository;
+        this.courseService = courseService;
+    }
+
+    // return all the courses
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+
+        List<Course> courses = courseService.getAllCourses();
+        if (courses != null && !courses.isEmpty()) {
+            return new ResponseEntity<>(courses, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    // return all information about a course using an id
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+        Course course = courseService.getCourseById(id);
+        if (course != null) {
+            return new ResponseEntity<>(course, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //API for creating a course
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        Course savedCourse = courseService.createCourse(course);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/courses/" + savedCourse.getId());
+        return new ResponseEntity<>(savedCourse, headers, HttpStatus.CREATED);
+    }
+
+    //API for deleting a course
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        courseService.deleteCourse(id);
+    }
+
+    //API for updating a specific course
+    @PutMapping("/{id}")
+    public Course updateCourse(@PathVariable("id") Long id, @RequestBody Course course) {
+        return courseService.updateCourse(id, course);
+    }
+
+}
+
+
---
+++ src/main/java/com/edu/m7/feedback/service/CourseService.java
@@ -0,0 +1,46 @@
+package com.edu.m7.feedback.service;
+import com.edu.m7.feedback.model.entity.Course;
+import com.edu.m7.feedback.model.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+    @Autowired
+    CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    // will execute a SELECT statement, retrieving all the courses in the database
+    public List<Course> getAllCourses(){
+        return courseRepository.findAll();
+    }
+
+    //will execute INSERT statement, because course object id is 'null'
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    //will execute an UPDATE statement, because course object is not 'null'
+    public Course updateCourse(Long id, Course course) {
+        course.setId(id);
+        return courseRepository.save(course);
+    }
+
+    // will execute a DELETE statement, targeting the specified id
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+
+    public Course getCourseById(Long id) {
+        Optional<Course> course = courseRepository.findById(id);
+        return course.orElse(null);
+    }
+
+}
