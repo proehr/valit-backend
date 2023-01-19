bb7099703170a00a030259a5126ed6172fca15fd
henry.ordelt <henry.ordelt@student.htw-berlin.de>
2023-01-19 08:51:25 +0100

    changed sign in endpoint to return lecturer details, full-register to return message

--- src/main/java/com/edu/m7/feedback/payload/response/JwtResponse.java
+++ src/main/java/com/edu/m7/feedback/payload/response/JwtResponse.java
@@ -1,5 +1,6 @@
 package com.edu.m7.feedback.payload.response;
 
+import com.edu.m7.feedback.model.entity.Lecturer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,11 +13,18 @@ public class JwtResponse {
     private String token;
     private String type = "Bearer";
     private String username;
-    private List<String> roles;
 
-    public JwtResponse(String token, String username, List<String> roles) {
+    private Long lecturerId;
+    private String title;
+    private String firstName;
+    private String lastName;
+
+    public JwtResponse(String token, String username, Long lecturerId, String title, String firstName, String lastName) {
         this.token = token;
         this.username = username;
-        this.roles = new ArrayList<>(roles);
+        this.lecturerId = lecturerId;
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 }
