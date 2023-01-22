edec0d7674893c2c7772a141d0d9c473800c7d6a
YasineDeghaies <yasine.deghaies@gmail.com>
2023-01-22 10:23:25 +0100

    imi-wise-2022/projectdocumentation#101 - getAllCourses method angepasst so dass ein Lecturer kann nur seine eigene Kurse sehen - weiter Änderungen folgen..

--- src/main/java/com/edu/m7/feedback/controller/CourseController.java
+++ src/main/java/com/edu/m7/feedback/controller/CourseController.java
@@ -1,7 +1,11 @@
 package com.edu.m7.feedback.controller;
 
+import com.edu.m7.feedback.model.entity.Account;
 import com.edu.m7.feedback.model.entity.Course;
+import com.edu.m7.feedback.model.entity.Lecturer;
+import com.edu.m7.feedback.model.repository.AccountRepository;
 import com.edu.m7.feedback.model.repository.CourseRepository;
+import com.edu.m7.feedback.model.repository.LecturerRepository;
 import com.edu.m7.feedback.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,20 +24,44 @@ public class CourseController {
 
     private final CourseService courseService;
     private final CourseRepository courseRepository;
+    private final LecturerRepository lecturerRepository;
+    private final AccountRepository accountRepository;
+
 
     @Autowired
     public CourseController(CourseService courseService,
-                            CourseRepository courseRepository) {
+                            CourseRepository courseRepository, LecturerRepository lecturerRepository, AccountRepository accountRepository) {
         this.courseService = courseService;
         this.courseRepository = courseRepository;
+        this.lecturerRepository = lecturerRepository;
+        this.accountRepository = accountRepository;
     }
 
     // return all the courses
-    @RolesAllowed({"ROLE_LECTURER", "ROLE_ADMIN"})
     @GetMapping
-    public ResponseEntity<List<Course>> getAllCourses() {
+    public ResponseEntity<List<Course>> getAllCourses(Principal principal) {
 
-        List<Course> courses = courseService.getAllCourses();
+        //get username of signed in account
+        String username = principal.getName();
+
+        //look for the account
+        Optional<Account> optionalEntity = accountRepository.findByUsername(username);
+
+        // return the logged in account Entity
+        Account account  = optionalEntity.orElseThrow();
+
+        // look for the lecturer associated with the account
+        Optional<Lecturer> optionalLecturer = lecturerRepository.findByAccount(account);
+
+        // return the associated lecturer entity
+        Lecturer lecturer = optionalLecturer.orElseThrow();
+
+        // get the id of the lecturer
+        Long lecturerId = lecturer.getLecturerId();
+
+        // look for the courses associated with the lecturer
+        List<Course> courses = courseService.getAllCourses(lecturerId);
+
         if (courses != null && !courses.isEmpty()) {
             return new ResponseEntity<>(courses, HttpStatus.OK);
         } else {
@@ -40,7 +69,7 @@ public class CourseController {
         }
     }
 
-    // return all information about a course using an id
+    // return a single course using it's id
     @RolesAllowed({"ROLE_LECTURER", "ROLE_ADMIN"})
     @GetMapping("/{id}")
     public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
--- src/main/java/com/edu/m7/feedback/model/repository/CourseRepository.java
+++ src/main/java/com/edu/m7/feedback/model/repository/CourseRepository.java
@@ -4,8 +4,11 @@ import com.edu.m7.feedback.model.entity.Course;
 import com.edu.m7.feedback.model.entity.Lecturer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByLecturer(Long lecturerId);
 }
