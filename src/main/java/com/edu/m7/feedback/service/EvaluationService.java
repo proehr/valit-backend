98b887a5dd772c72c23b88167fdce63074bd5539
YasineDeghaies <yasine.deghaies@gmail.com>
2023-01-26 20:23:04 +0100

    imi-wise-2022/projectdocumentation#101 - getLecturer im EvaluationService durch getLecturerByEvaulationId ersetzt - generic type parameter wieder geandert

--- src/main/java/com/edu/m7/feedback/service/EvaluationService.java
+++ src/main/java/com/edu/m7/feedback/service/EvaluationService.java
@@ -1,15 +1,17 @@
 package com.edu.m7.feedback.service;
 
-import com.edu.m7.feedback.model.dto.CourseDto;
 import com.edu.m7.feedback.model.dto.EvaluationDto;
 import com.edu.m7.feedback.model.entity.Course;
 import com.edu.m7.feedback.model.mapping.CourseDtoMapper;
 import com.edu.m7.feedback.model.mapping.EvaluationDtoMapper;
+import com.edu.m7.feedback.model.repository.CourseRepository;
 import com.edu.m7.feedback.model.repository.EvaluationRepository;
+import com.edu.m7.feedback.model.repository.LecturerRepository;
 import org.mapstruct.factory.Mappers;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EvaluationService {
@@ -17,9 +19,14 @@ public class EvaluationService {
     private static final EvaluationDtoMapper mapper = Mappers.getMapper(EvaluationDtoMapper.class);
     private static final CourseDtoMapper courseMapper = Mappers.getMapper(CourseDtoMapper.class);
 
+    private final LecturerRepository lecturerRepository;
+    private final CourseRepository courseRepository;
+
     private final EvaluationRepository repository;
 
-    public EvaluationService(EvaluationRepository repository) {
+    public EvaluationService(LecturerRepository lecturerRepository, CourseRepository courseRepository, EvaluationRepository repository) {
+        this.lecturerRepository = lecturerRepository;
+        this.courseRepository = courseRepository;
         this.repository = repository;
     }
 
@@ -29,6 +36,10 @@ public class EvaluationService {
 
     public Course getCourse(Long id) {
             return repository.findById(id).get().getCourse();
+    }
+    public Long getLecturerIdByEvaluationId(Long id) {
+        Optional<Course> optionalCourse = courseRepository.findById(id);
+        return optionalCourse.map(course -> course.getLecturer().getLecturerId()).orElseThrow();
     }
 
 }
