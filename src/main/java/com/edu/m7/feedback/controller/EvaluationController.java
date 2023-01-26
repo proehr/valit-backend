44fae7ae9eafd7835a528097f77e97f7ec39979f
YasineDeghaies <yasine.deghaies@gmail.com>
2023-01-26 10:44:10 +0100

    imi-wise-2022/projectdocumentation#101 - getEvaluation secured via principal

--- src/main/java/com/edu/m7/feedback/controller/EvaluationController.java
+++ src/main/java/com/edu/m7/feedback/controller/EvaluationController.java
@@ -1,7 +1,15 @@
 package com.edu.m7.feedback.controller;
 
+import com.edu.m7.feedback.model.dto.CourseDto;
 import com.edu.m7.feedback.model.dto.EvaluationDto;
+import com.edu.m7.feedback.model.entity.Course;
+import com.edu.m7.feedback.model.entity.Lecturer;
+import com.edu.m7.feedback.model.repository.EvaluationRepository;
+import com.edu.m7.feedback.payload.response.MessageResponse;
+import com.edu.m7.feedback.service.CourseService;
 import com.edu.m7.feedback.service.EvaluationService;
+import com.edu.m7.feedback.service.LecturerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +23,16 @@ import java.security.Principal;
 @RequestMapping("/api/evaluations")
 public class EvaluationController {
     private final EvaluationService evaluationService;
+    private final LecturerService lecturerService;
+    private final CourseService courseService;
 
-    public EvaluationController(EvaluationService evaluationService) {
+    private final EvaluationRepository evaluationRepository;
+
+    public EvaluationController(EvaluationService evaluationService, LecturerService lecturerService, CourseService courseService, EvaluationRepository evaluationRepository) {
         this.evaluationService = evaluationService;
+        this.lecturerService = lecturerService;
+        this.courseService = courseService;
+        this.evaluationRepository = evaluationRepository;
     }
 
     //Get questions for Student
@@ -25,8 +40,17 @@ public class EvaluationController {
     //Get Questions + Answers for Lecturer
     @RolesAllowed({"ROLE_LECTURER", "ROLE_ADMIN"})
     @GetMapping("/{id}/summary")
-    ResponseEntity<EvaluationDto> getEvaluation(@PathVariable Long id, Principal principal) {
+    ResponseEntity<?> getEvaluation(@PathVariable Long id, Principal principal) {
         //TODO secure this via principal
+        Lecturer principalLecturer = lecturerService.getLecturer(principal);
+        Course relatedCourse = evaluationService.getCourse(id);
+        Lecturer lecturer = relatedCourse.getLecturer();
+
+        if(!principalLecturer.equals(lecturer) ){
+                return new ResponseEntity<>(
+                        new MessageResponse("Access to the requested Evaluation is not allowed"), HttpStatus.UNAUTHORIZED);
+        }
+
         return ResponseEntity.ok(evaluationService.loadEvaluationById(id));
     }
 
--- src/main/java/com/edu/m7/feedback/service/EvaluationService.java
+++ src/main/java/com/edu/m7/feedback/service/EvaluationService.java
@@ -1,9 +1,13 @@
 package com.edu.m7.feedback.service;
 
+import com.edu.m7.feedback.model.dto.CourseDto;
 import com.edu.m7.feedback.model.dto.EvaluationDto;
+import com.edu.m7.feedback.model.entity.Course;
+import com.edu.m7.feedback.model.mapping.CourseDtoMapper;
 import com.edu.m7.feedback.model.mapping.EvaluationDtoMapper;
 import com.edu.m7.feedback.model.repository.EvaluationRepository;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +15,8 @@ import org.springframework.stereotype.Service;
 public class EvaluationService {
 
     private static final EvaluationDtoMapper mapper = Mappers.getMapper(EvaluationDtoMapper.class);
+    private static final CourseDtoMapper courseMapper = Mappers.getMapper(CourseDtoMapper.class);
+
     private final EvaluationRepository repository;
 
     public EvaluationService(EvaluationRepository repository) {
@@ -20,4 +26,9 @@ public class EvaluationService {
     public EvaluationDto loadEvaluationById(Long id) throws UsernameNotFoundException {
         return mapper.entityToDto(repository.findById(id).orElseThrow());
     }
+
+    public Course getCourse(Long id) {
+            return repository.findById(id).get().getCourse();
+    }
+
 }
