61ea6cfc10e7f442b375f238e0525ec59c0e970f
Pauline <pauline.roehr@student.htw-berlin.de>
2023-02-01 23:17:34 +0100

    imi-wise-2022/projectdocumentation#88 - add websockets for live feedback

---
+++ src/test/resources/js/websocket-test.js
@@ -0,0 +1,46 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('http://localhost:8080/live-feedback');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        // 1 should be replaced by the course shortcode
+        stompClient.subscribe('/queue/1', function (message) {
+            showMessage(JSON.parse(message.body).messageType);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function showMessage(message) {
+    $("#messages").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+});
