d51fac2a7d129cb60e105dbf89eff7feda738da0
Pauline <pauline.roehr@student.htw-berlin.de>
2023-02-01 15:46:37 +0100

    imi-wise-2022/projectdocumentation#144 - fix cross origin issues for all controllers & some small refactorings

--- src/main/java/com/edu/m7/feedback/controller/EvaluationController.java
+++ src/main/java/com/edu/m7/feedback/controller/EvaluationController.java
@@ -6,6 +6,7 @@ import com.edu.m7.feedback.service.LecturerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import java.util.NoSuchElementException;
 @RestController
 @RequestMapping("/api/evaluations")
 @Slf4j
+@CrossOrigin
 public class EvaluationController {
     private final EvaluationService evaluationService;
     private final LecturerService lecturerService;
