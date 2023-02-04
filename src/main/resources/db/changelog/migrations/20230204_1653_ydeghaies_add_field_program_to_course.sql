013bf54e6cdeab56393a5918ad292657d6e7d734
YasineDeghaies <yasine.deghaies@gmail.com>
2023-02-04 17:09:23 +0100

    imi-wise-2022/projectdocumentation#158 - fixed das format der Bennenung

--- src/main/resources/db/changelog/migrations/20230204_1653_ydeghaies_add_field_program_to_course.sql
+++ src/main/resources/db/changelog/migrations/20230204_1653_ydeghaies_add_field_program_to_course.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset 20230204_1653_ydeghaies_add_field_program_to_course
+-- changeset liquibase:20230204_1653_ydeghaies_add_field_program_to_course
 ALTER TABLE course
     ADD program VARCHAR(255);
 
