42ebfc5e6609ce8f44ff3d901ebcb9a3e3908efa
Pauline <pauline.roehr@student.htw-berlin.de>
2023-02-10 00:38:51 +0100

    no-ticket - fix path for messaging endpoints

--- src/main/java/com/edu/m7/feedback/controller/WebsocketController.java
+++ src/main/java/com/edu/m7/feedback/controller/WebsocketController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
@@ -20,6 +21,7 @@ import java.security.Principal;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/api")
 public class WebsocketController {
 
     private final AccountService accountService;
